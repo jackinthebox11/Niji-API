5c220600afb14ba272c6cc98ef3e9662de09dadf
samy35135 <samy.abdallah.fr@gmail.com>
2015-03-24 16:32:06 +0100

    commit peu important pour être à jour

--- DAD_API_REST/dad_api_rest/src/main/java/client/Client.java
+++ DAD_API_REST/dad_api_rest/src/main/java/client/Client.java
@@ -13,7 +13,7 @@ import factory.FactoryUser;
 public class Client {
 	public static void main(String[] args) {
 		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory("persiste");
+				.createEntityManagerFactory("example");
 		EntityManager manager = factory.createEntityManager();
 		System.out.println("Launch of the Client...");
 		FactoryUser factoryuser = new FactoryUser(manager);
